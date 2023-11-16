4bdb04ca7b2c2dd46ed67b3c419a2cf8b50b4cd5
isiji <blairisiji@gmail.com>
2023-11-16 08:42:21 +0000

    pushpall

--- main.c
+++ main.c
@@ -1,43 +1,47 @@
-#include "monty.h"
+#define _GNU_SOURCE
 #include <stdio.h>
-
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+/**
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	char *content;
 	FILE *file;
 	size_t size = 0;
-	ssize_t input_line = 1;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-	unsigned int len = 0;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	bus.file = file;
-
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (input_line > 0)
+	while (read_line > 0)
 	{
 		content = NULL;
-		input_line = getline(&content, &size, file);
+		read_line = getline(&content, &size, file);
 		bus.content = content;
-		len++;
-
-		if (input_line > 0)
+		counter++;
+		if (read_line > 0)
 		{
-			execute(content, &stack, len, file);
+			execute(content, &stack, counter, file);
 		}
 		free(content);
-	
 	}
 	free_stack(stack);
 	fclose(file);
-	return (0);
+return (0);
 }
